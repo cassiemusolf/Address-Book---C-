fbef339e4116dabbbd184adc1e7fcd9e43946cf5
Cassie Musolf <cassiemusolf@gmail.com>
2017-02-10 12:15:36 -0800

    try different ways to make contact name show only and have it clickable in order to take user to a seperate page with details.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,21 +11,31 @@ namespace AddressBook
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
-      Get["/contacts"] = _ => {
+      Get["/"] = _ => {
         List<Contact> allContacts = Contact.GetAll();
         return View["index.cshtml", allContacts];
       };
-      Get["/contacts/new"] = _ => {
+      Get["/contacts"] = _ => {
         return View["contact_form.cshtml"];
       };
-      Post["/contacts"] = _ => {
+      Get["/contacts/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedContact = Contact.Find(parameters.id);
+        model.Add("contact", selectedContact);
+        // model.Add("albums", artistAlbums);
+        return View["contacts.cshtml", model];
+      };
+      Post["/contact/new"] = _ => {
+        return View["contact_added.cshtml"];
+      };
+      Post["/"] = _ => {
         Contact newContact = new Contact(Request.Form["contact-name"], Request.Form["contact-address"], Request.Form["contact-phone"]);
         List<Contact> allContacts = Contact.GetAll();
-        return View["contacts.cshtml", allContacts];
+        return View["/", allContacts];
       };
       Post["/contacts/clear"] = _ => {
-      Contact.ClearAll();
-      return View["contacts_cleared.cshtml"];
+        Contact.ClearAll();
+        return View["contacts_cleared.cshtml"];
       };
     }
   }
