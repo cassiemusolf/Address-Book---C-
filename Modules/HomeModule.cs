9dcb62600071aeadf9904d0ccef941578472c35b
Cassie Musolf <cassiemusolf@gmail.com>
2017-02-10 15:15:39 -0800

    make changes to capitalization and indentation on all pages

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -18,20 +18,15 @@ namespace AddressBook
       Get["/contacts/new"] = _ => {
         return View["contact_form.cshtml"];
       };
-
       Get["/contacts/{id}"] = parameters => {
         Contact contact = Contact.Find(parameters.id);
         return View["/contact.cshtml", contact];
         };
-      // Post["/contact/new"] = _ => {
-      //   return View["contact_added.cshtml"];
-      // };
       Post["/contacts"] = _ => {
         Contact newContact = new Contact(Request.Form["contact-name"], Request.Form["contact-address"], Request.Form["contact-phone"]);
         List<Contact> allContacts = Contact.GetAll();
         return View["contacts.cshtml", allContacts];
       };
-
       Post["/contacts/clear"] = _ => {
         Contact.ClearAll();
         return View["contacts_cleared.cshtml"];
